2bfae6ba5e947c185159fbd8144b08fa51895303
Nikita Justin Olechko <96552468+nikita-olechko@users.noreply.github.com>
2023-02-25 14:48:17 -0800

    Update Get_distance.py

--- Get_distance.py
+++ Get_distance.py
@@ -1,10 +1,15 @@
-from mindstorms import DistanceSensor
+from mindstorms import DistanceSensor, MSHub, Motor, MotorPair, ColorSensor, App
 
 # Initialize the Distance Sensor.
+hub = MSHub()
 wall_detector = DistanceSensor('F')
-
 # Measure the distance between the Distance Sensor and object in centimeters and inches.
 dist_cm = wall_detector.get_distance_cm()
 dist_inches = wall_detector.get_distance_inches()
 # Print both results on the console.
 print('cm:', dist_cm, 'inches:', dist_inches)
+hub.speaker.play_sound("Damage")
+if dist_cm > 10:
+    hub.speaker.play_sound("Damage")
+else:
+    hub.speaker.play_sound("Celebrate")
